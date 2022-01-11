7b3b2d939dc29cb0893a0a64d25e482d3594d472
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-11 18:04:58 -0500

    commit tree 9505a1343f70a7867bf8056668056a389ad6f9f9

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+"strict";
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = function (driverArray) {
+  return driverArray.slice(0, 2);
+};
+const returnLastTwoDrivers = function (driverArray) {
+  return driverArray.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+  return function (multiplier) {
+    return fare * multiplier;
+  };
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, someFunction) {
+  // console.log('DRIVERS', drivers)
+  // console.log('FUNC'), func
+  return someFunction(arrayOfDrivers);
+}
+// createFareMultiplier() — This is a higher-order function that takes in one argument, an integer, and returns a function that will multiply a fare for a ride accordingly. If createFareMultiplier() receives an argument of 4, it will return a function that takes in a fare as an argument and quadruples the fare.
